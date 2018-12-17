c0309d43570d6003ccb1bfff24594d8928470daa
nikki-thn <ntruong4@myseneca.ca>
2018-12-17 18:38:05 -0500

    Add files via upload

---
+++ Assignment4/Controllers/Invoice_vm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace INT422_Assignment4.Controllers
+{
+	public class InvoiceBase
+	{
+		[Key]
+        public int InvoiceId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public DateTime InvoiceDate { get; set; }
+
+        [StringLength(70)]
+        public string BillingAddress { get; set; }
+
+        [StringLength(40)]
+        public string BillingCity { get; set; }
+
+        [StringLength(40)]
+        public string BillingState { get; set; }
+
+        [StringLength(40)]
+        public string BillingCountry { get; set; }
+
+        [StringLength(10)]
+        public string BillingPostalCode { get; set; }
+
+        public decimal Total { get; set; }
+    }
+}
---
+++ Assignment4/Controllers/InvoicesController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INT422_Assignment4.Controllers
+{
+    public class InvoicesController : Controller
+    {
+        // GET: Invoices
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Invoices/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Invoices/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Invoices/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Invoices/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Invoices/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Invoices/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Invoices/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
